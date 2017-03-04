b8a46018f6fd9de59bc6261782b4477545db5ec3
jennyrieck <jenny.rieck@gmail.com>
2017-03-04 14:06:41 -0500

    added subject # print to terminal

--- MARINeR/R/subject.data.list.R
+++ MARINeR/R/subject.data.list.R
@@ -44,6 +44,7 @@ subject.data.list <- function(subj.list){
   names(masked.subject.data) <- names(subj.list)
 
   for(s in 1:length(subj.list)){
+    print(paste('running subject#', s, 'of', length(subj.list)))
     masked.subject.data[[s]] <- volsToMatrix(subj.list[[s]]$data, subj.list[[s]]$masks,subj.list[[s]]$design)
   }
 
