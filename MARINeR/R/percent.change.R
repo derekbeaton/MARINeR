86b148135c03b38f82cb26a9e143ef76ed4f6ee6
derek <you@example.com>
2017-11-18 18:15:59 -0500

    update to percent change
    for block designs

--- MARINeR/R/percent.change.R
+++ MARINeR/R/percent.change.R
@@ -1,5 +1,6 @@
 
-
+	### will need a bit more work but I think this is pretty good.
+	
 percent.change <- function(data,onsets){
 
 
@@ -7,14 +8,30 @@ percent.change <- function(data,onsets){
 	rep.baselines <- unlist(onsets[which(onsets[,1]=="BLOCK_LENGTH"),3:ncol(onsets)])
 	endpoints <- (baselines-1) + rep.baselines
 	
+	event.design <- 	 rep(rep(unique(onsets[,2]),length(3:ncol(onsets))),rep.baselines)	
 	perc.change.data <- (data[min(baselines):max(endpoints),] - data[rep(baselines,rep.baselines),]) / data[rep(baselines,rep.baselines),]
-		## baselines here provide the "design"
-		# make design based on baselines
-			### this needs to be the weighted design ala BADA.
+	
+	perc.change.data <- perc.change.data[-c(baselines),]
+	event.design <- event.design[-c(baselines)]
+	
+
+	
+	block.design <- makeNominalData(as.matrix( rep(paste(rep(unique(onsets[,2]),length(3:ncol(onsets))),rep(1:length(3:ncol(onsets)),each=length(unique(onsets[,2]))),sep="__.__"),rep.baselines) ))
+	
+	
+	block.design <- block.design[-c(baselines),]
+	block.design <- t(apply(block.design, 1, "/", colSums(block.design)))
 	ave.perc.change.data <- t(block.design) %*% perc.change.data
 	rownames(ave.perc.change.data) <- colnames(block.design)
 	
-	return(list(perc.change.data= perc.change.data,ave.perc.change.data=ave.perc.change.data))
+	ave.row.design <- unlist(lapply(strsplit(gsub("\\.","",rownames(ave.perc.change.data)),"____"),function(x){x[1]}))
+	
+	
+	return(list( 	perc.change.data = perc.change.data, 
+					event.design = event.design, 
+					ave.perc.change.data=ave.perc.change.data, 
+					ave.row.design= ave.row.design
+				))
 	
 	
 }
